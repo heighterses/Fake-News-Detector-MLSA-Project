f43d5a36b6071ed1f31562727e9baa343557afcc
M. Abdullah Bin Aftab <heighter79@gmail.com>
2024-05-24 23:41:19 +0500

    adding styling and GUI buttons for navigations

--- app.py
+++ app.py
@@ -1,11 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+def home():
+    return render_template("home.html")
+
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
 
 
 if __name__ == '__main__':
